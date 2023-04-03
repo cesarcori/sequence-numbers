13aed46541183c66acca1ae57a4b0cd89f8d2c32
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-04-03 15:05:06 -0400

    Feat: algorithm fibonacci

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 function fibonacciNumber() {
-  let fibo_number = document.getElementById("enter-value");
+  let fibo_number = document.getElementById("enter-value").value;
+
+  let n1 = 0, n2 = 1, nextTerm; // variables
+
+  for (let i = 1; i <= fibo_number; i++) {
+      document.getElementById("result-fibonacci").innerText = n1
+      console.log(n1)
+      nextTerm = n1 + n2;
+      n1 = n2;
+      n2 = nextTerm;
+  }
 }
